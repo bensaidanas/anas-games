802e21b4b74db63bb0f639028f7689f53783f206
Anas Bensaid <bensaidanass9@gmail.com>
2023-09-23 20:11:59 +0100

    implementing the filter functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+        <Text fontSize={"3xl"}>Anas Games</Text>
+      </HStack>
       <ColorModeSwitch />
     </HStack>
   );
